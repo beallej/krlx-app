b469fa6ffe6e0328e7ca67cfb01d53c60bb48f0d
Josie Bealle <beallej@carleton.edu>
2015-05-12 14:02:09 -0500

    what even

--- KRLX/KRLX/NewsTableViewController.swift
+++ KRLX/KRLX/NewsTableViewController.swift
@@ -18,6 +18,43 @@ class NewsTableViewController: UITableViewController {
             menuButton.action = "revealToggle:"
             self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
         }
+        let html = "<html><head></head><body><ul><li><input type='image' name='input1' value='string1value' class='abc' /></li><li><input type='image' name='input2' value='string2value' class='def' /></li></ul><span class='spantext'><b>Hello World 1</b></span><span class='spantext'><b>Hello World 2</b></span><a href='example.com'>example(English)</a><a href='example.co.jp'>example(JP)</a></body>"
+        
+        var err : NSError?
+        var parser     = HTMLParser(html: html, error: &err)
+        if err != nil {
+            println(err)
+            exit(1)
+        }
+        
+        var bodyNode   = parser.body
+        
+        if let inputNodes = bodyNode?.findChildTags("b") {
+            for node in inputNodes {
+                println(node.contents)
+            }
+        }
+        
+        if let inputNodes = bodyNode?.findChildTags("a") {
+            for node in inputNodes {
+                println(node.contents)
+                println(node.getAttributeNamed("href"))
+            }
+        }
+        let myURLString = "http://www.krlx.org/"
+        
+        if let myURL = NSURL(string: myURLString) {
+            var error: NSError?
+            let myHTMLString = NSString(contentsOfURL: myURL, encoding: NSUTF8StringEncoding, error: &error)
+            
+            if let error = error {
+                println("Error : \(error)")
+            } else {
+                //println("HTML : \(myHTMLString)")
+            }
+        } else {
+            println("Error: \(myURLString) doesn't seem to be a valid URL")
+        }
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
