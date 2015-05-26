072cb77e16e0288f6dbf9318ec98fc5e5a44dc8d
Josie Bealle <beallej@carleton.edu>
2015-05-26 14:12:08 -0500

    now uses synchronous request, all the stuff is in google api pull :)

--- KRLX/KRLX/GoogleAPIPull.swift
+++ KRLX/KRLX/GoogleAPIPull.swift
@@ -8,19 +8,17 @@
 
 import Foundation
 class GoogleAPIPull {
-    init(){
-    }
+    var show_arrays : [ShowHeader]
 
-    /*
-    func pullKRLXGoogleCal() {
-        // This function pull KRLX calendar using Google Calendar API,
-        // parse info and display it in readable form
-        
+    init(){
+        show_arrays = [ShowHeader]()
+
+    }
+    func getCurrentShow() -> ShowHeader?{
+        var currentShow: ShowHeader?
         let curTime = getCurrentTime()
-        var urlString = "https://www.googleapis.com/calendar/v3/calendars/cetgrdg2sa8qch41hsegktohv0%40group.calendar.google.com/events?singleEvents=true&orderBy=startTime&timeMin="+curTime+"Z&timeZone=America%2fChicago&maxResults=60&key=AIzaSyD-Lcm54auLNoxEPqxNYpq2SP4Jcldzq2I"
+        var urlString = "https://www.googleapis.com/calendar/v3/calendars/cetgrdg2sa8qch41hsegktohv0%40group.calendar.google.com/events?singleEvents=true&orderBy=startTime&timeMin="+curTime+"Z&timeZone=America%2fChicago&maxResults=1&key=AIzaSyD-Lcm54auLNoxEPqxNYpq2SP4Jcldzq2I"
         let url = NSURL(string: urlString)
-        //
-        //let url = NSURL(string: "https://www.googleapis.com/calendar/v3/calendars/cetgrdg2sa8qch41hsegktohv0%40group.calendar.google.com/events?singleEvents=true&orderBy=startTime&timeMin=2015-05-23T10%3A44%3A59Z&timeZone=America%2FChicago&maxResults=100&key=AIzaSyD-Lcm54auLNoxEPqxNYpq2SP4Jcldzq2I")
         
         
         var request : NSMutableURLRequest = NSMutableURLRequest()
@@ -29,13 +27,78 @@ class GoogleAPIPull {
         var response: NSURLResponse?
         var error: NSError?
         
+        //NSURLConnection used here because NSURLSession does not have a synchronous request option,  which we need in this case.
+        let urlData = NSURLConnection.sendSynchronousRequest(request, returningResponse: &response, error: &error)
         
-        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue(), completionHandler:{ (response:NSURLResponse!, data: NSData!, error: NSError!) -> Void in
+        if let data = urlData{
+            
             var errorJSON: AutoreleasingUnsafeMutablePointer<NSError?> = nil
-            if (data != nil){
-                
+            
+            let jsonResult: NSDictionary! = NSJSONSerialization.JSONObjectWithData(data, options:NSJSONReadingOptions.MutableContainers, error: errorJSON) as? NSDictionary
+            // Parse show content of NSDictionary
+            if (jsonResult != nil){
+                if let allitems_wrapper = jsonResult["items"] as? NSArray{
+                    for item in allitems_wrapper {
+                        var ShowTitle = item["summary"] as! String
+                        var startTimeArray = (item["start"] as! NSDictionary)["dateTime"] as! String
+                        var startTime = self.prettifyTimeLabel(startTimeArray)[0]
+                        var date = self.prettifyTimeLabel(startTimeArray)[1] as String
+                        var endTime = (item["end"] as! NSDictionary)["dateTime"] as! String
+                        endTime = self.prettifyTimeLabel(endTime)[0]
+                        var ShowDJ: String
+                        if let DJ:String = item["description"] as? String {
+                            ShowDJ = DJ
+                        }
+                        else {
+                            ShowDJ = ""
+                        }
+                        var show = ShowHeader(titleString: ShowTitle, startString: startTime, endString: endTime, DJString: ShowDJ, dateString: date)
+                        currentShow = show
+                        
+                    }
+                }
+            }
+            else
+            {
+                //currentShow never assigned
+            }
+        }
+            
+            //no response
+        else{
+            //currentShow never assigned
+            
+        }
+        return currentShow
+    }
+    
+    func pullKRLXGoogleCal() -> Bool?{
+        // This function pull KRLX calendar using Google Calendar API,
+        // parse info and display it in readable form
+        var needsReload : Bool?
+        
+        let curTime = getCurrentTime()
+        var urlString = "https://www.googleapis.com/calendar/v3/calendars/cetgrdg2sa8qch41hsegktohv0%40group.calendar.google.com/events?singleEvents=true&orderBy=startTime&timeMin="+curTime+"Z&timeZone=America%2fChicago&maxResults=60&key=AIzaSyD-Lcm54auLNoxEPqxNYpq2SP4Jcldzq2I"
+        let url = NSURL(string: urlString)
+
+        
+        
+        var request : NSMutableURLRequest = NSMutableURLRequest()
+        request.URL = url
+        request.HTTPMethod = "GET"
+        var response: NSURLResponse?
+        var error: NSError?
+
+        //NSURLConnection used here because NSURLSession does not have a synchronous request option,  which we need in this case.
+        let urlData = NSURLConnection.sendSynchronousRequest(request, returningResponse: &response, error: &error)
+        
+        if let data = urlData{
+        
+            var errorJSON: AutoreleasingUnsafeMutablePointer<NSError?> = nil
+            
                 let jsonResult: NSDictionary! = NSJSONSerialization.JSONObjectWithData(data, options:NSJSONReadingOptions.MutableContainers, error: errorJSON) as? NSDictionary
-                // Parse show content of NSDictionary
+            
+            // Parse show content of NSDictionary
                 if (jsonResult != nil){
                     if let allitems_wrapper = jsonResult["items"] as? NSArray{
                         for item in allitems_wrapper {
@@ -60,32 +123,36 @@ class GoogleAPIPull {
                     var newShowList = NSMutableArray(array: self.show_arrays)
                     if newShowList != sharedData.loadedShowHeaders{
                         sharedData.loadedShowHeaders = newShowList
-                        self.setFirstShow()
-                        self.tableView?.reloadData()
-                        
+                        needsReload = true
+  
                     }
-                    self.spinnyWidget.stopAnimating()
+                    else{
+                        needsReload = false
+
+                    }
                 }
                     
                 else
                 {
-                    self.currentShowLabel.text = "Whoops! Something's wrong with the server"
-                    self.spinnyWidget.stopAnimating()
+                    //needsReload never assigned
                 }
             }
                 
                 //no response
             else{
-                self.currentShowLabel.text = "No internet connection"
-                self.spinnyWidget.stopAnimating()
+            //needsReload never assigned
+
             }
-        })
-    }
+        return needsReload
+
+        }
     
+    
+    // This function take in a time string of format 2015-05-21T05:30:00-05:00
+    // Return an array of prettified [time, date]
     func prettifyTimeLabel(time: String) -> [String]
-        // This function take in a time string of format 2015-05-21T05:30:00-05:00
-        // Return an array of prettified [time, date]
     {
+        
         var finalTime : [String] = []
         //2015-05-21T05:30:00-05:00 --> 05:30:00
         var tm = time.componentsSeparatedByString("T")[1].componentsSeparatedByString("-")[0]
@@ -113,7 +180,7 @@ class GoogleAPIPull {
         finalTime.append(date)
         return finalTime
         
-}
+    }
     func getCurrentTime () -> String{
         // Get current time in UTC to substitute in the Google API String
         // timeMin=2015-05-23T10%3A44%3A59Z&timeZone=America%2FChicago
@@ -123,6 +190,6 @@ class GoogleAPIPull {
         dateFormatter.timeZone = NSTimeZone(abbreviation: "UTC")
         var dateString = dateFormatter.stringFromDate(now) as String
         return dateString
-}
-*/
+    }
+
 }
--- KRLX/KRLX/LiveStreamViewController.swift
+++ KRLX/KRLX/LiveStreamViewController.swift
@@ -25,7 +25,7 @@ class LiveStreamViewController: UIViewController, AVAudioPlayerDelegate {
     
     override func viewDidLoad() {
         self.currentShowLabel.text = ""
-        getCurrentShow()
+        self.setCurrentShow()
         var appDelegate: AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
         if appDelegate.isPlaying {
             let image = UIImage(named: "pause") as UIImage?
@@ -76,115 +76,12 @@ class LiveStreamViewController: UIViewController, AVAudioPlayerDelegate {
         // Dispose of any resources that can be recreated.
     }
     
-    func getCurrentShow(){
-        var labelText: String = ""
-        // This function pull KRLX calendar using Google Calendar API,
-        // parse info and display it in readable form
-        
-        let curTime = getCurrentTime()
-        var urlString = "https://www.googleapis.com/calendar/v3/calendars/cetgrdg2sa8qch41hsegktohv0%40group.calendar.google.com/events?singleEvents=true&orderBy=startTime&timeMin="+curTime+"Z&timeZone=America%2fChicago&maxResults=60&key=AIzaSyD-Lcm54auLNoxEPqxNYpq2SP4Jcldzq2I"
-        let url = NSURL(string: urlString)
-        
-        var request : NSMutableURLRequest = NSMutableURLRequest()
-        request.URL = url
-        request.HTTPMethod = "GET"
-        var response: NSURLResponse?
-        var error: NSError?
-        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue(), completionHandler:{ (response:NSURLResponse!, data: NSData!, error: NSError!) -> Void in
-            var errorJSON: AutoreleasingUnsafeMutablePointer<NSError?> = nil
-            if (data != nil){
-                let jsonResult: NSDictionary! = NSJSONSerialization.JSONObjectWithData(data, options:NSJSONReadingOptions.MutableContainers, error: errorJSON) as? NSDictionary
-                // Parse show content of NSDictionary
-                if (jsonResult != nil){
-                    if let allitems_wrapper = jsonResult["items"] as? NSArray{
-                        for index in 0...0{
-                            var ShowTitle = allitems_wrapper[0]["summary"] as! String
-                            var startTimeArray = (allitems_wrapper[0]["start"] as! NSDictionary)["dateTime"] as! String
-                            var startTime = self.prettifyTimeLabel(startTimeArray)[0]
-                            var date = self.prettifyTimeLabel(startTimeArray)[1] as String
-                            var endTime = (allitems_wrapper[0]["end"] as! NSDictionary)["dateTime"] as! String
-                            endTime = self.prettifyTimeLabel(endTime)[0]
-                            var ShowDJ: String
-                            if let DJ:String = allitems_wrapper[0]["description"] as? String {
-                                ShowDJ = DJ
-                            }
-                            else {
-                                ShowDJ = ""
-                            }
-                            var show = ShowHeader(titleString: ShowTitle, startString: startTime, endString: endTime, DJString: ShowDJ, dateString: date)
-                            self.show_arrays.append(show)
-                        }
-                    }
-                    var newShowList = NSMutableArray(array: self.show_arrays)
-                    if newShowList != sharedData.loadedShowHeaders{
-                        sharedData.loadedShowHeaders = newShowList
-                        self.setCurrentShow()
-                        
-                    }
-                }
-                    
-                else
-                {
-                    self.currentShowLabel.text = "Whoops! Something's wrong with the server"
-                }
-            }
-                
-                //no response
-            else{
-                self.currentShowLabel.text = "No internet connection"
-            }
-        })
-    }
-
-    
-    func getCurrentTime () -> String{
-        // Get current time in UTC to substitute in the Google API String
-        // timeMin=2015-05-23T10%3A44%3A59Z&timeZone=America%2FChicago
-        let now = NSDate()
-        var dateFormatter = NSDateFormatter()
-        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH'%3A'mm'%3A'ss"
-        dateFormatter.timeZone = NSTimeZone(abbreviation: "UTC")
-        var dateString = dateFormatter.stringFromDate(now) as String
-        return dateString
-    }
-    
-    func prettifyTimeLabel(time: String) -> [String]
-        // This function take in a time string of format 2015-05-21T05:30:00-05:00
-        // Return an array of prettified [time, date]
-    {
-        var finalTime : [String] = []
-        //2015-05-21T05:30:00-05:00 --> 05:30:00
-        var tm = time.componentsSeparatedByString("T")[1].componentsSeparatedByString("-")[0]
-        var date = time.componentsSeparatedByString("T")[0]
-        
-        //05:30:00 --> 5:30
-        var hour = tm.substringWithRange(Range<String.Index>(start: tm.startIndex, end: advance(tm.endIndex, -6)))
-        var minute = tm.substringWithRange(Range<String.Index>(start: advance(tm.startIndex, 2), end: advance(tm.endIndex, -3)))
-        var hourNumber = hour.toInt()
-        
-        //Convert 24 hour scale to 12 hour scale
-        if hourNumber > 12 {
-            hourNumber = hourNumber! % 12
-            finalTime.append(hourNumber!.description + minute + "pm")
-        }
-        else if hourNumber == 12 {
-            finalTime.append(hourNumber!.description + minute + "pm")
-        }
-        else if hourNumber == 0 {
-            finalTime.append("12" + minute + "am")
-        }
-        else{
-            finalTime.append(hourNumber!.description+minute+"am")
-        }
-        finalTime.append(date)
-        return finalTime
-        
-    }
-
-
     func setCurrentShow(){
-        let show = sharedData.loadedShowHeaders[0] as! ShowHeader
-        self.currentShowLabel.text = "Listening to " + show.getTitle() + " by " + show.getDJ()
+        let calendarAssistant = GoogleAPIPull()
+        if let currentShow = calendarAssistant.getCurrentShow(){
+            self.currentShowLabel.text = "Listening to " + currentShow.getTitle() + " by " + currentShow.getDJ()
+        }
+        // if nothing returned from server, label is blank
     }
     
 }
